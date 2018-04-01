52d35bf6d180f0edc2a39f428e57130af4646765
wanghuidong <zlt121030@163.com>
2018-04-01 10:05:54 +0800

    配置注册eureka注册中心

---
+++ eureka-server-01/src/main/java/com/whd/EurekaServer01App.java
@@ -0,0 +1,14 @@
+package com.whd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer01App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer01App.class, args);
+	}
+}
